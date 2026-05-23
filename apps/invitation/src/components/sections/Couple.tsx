d2cea9a21c9fde282d47774b2669d77be716bc86
hartantaadrian <35249226+hartantaadrian@users.noreply.github.com>
2026-05-23 15:21:06 +0700

    feat(dega-ditta): tilt bouquet left off the faces

--- apps/invitation/src/components/sections/Couple.tsx
+++ apps/invitation/src/components/sections/Couple.tsx
@@ -67,7 +67,7 @@ export default function Couple({
                 {centerPhoto && <img src={centerPhoto} alt={`${groomName} & ${brideName}`} className="w-full h-full object-cover" />}
               </div>
               {bouquetImage && (
-                <img src={bouquetImage} alt="" aria-hidden className="absolute -left-14 sm:-left-20 -bottom-14 w-32 sm:w-40 object-contain pointer-events-none" style={{ transform: 'scaleX(-1)' }} />
+                <img src={bouquetImage} alt="" aria-hidden className="absolute -left-14 sm:-left-20 -bottom-14 w-32 sm:w-40 object-contain pointer-events-none" style={{ transform: 'scaleX(-1) rotate(12deg)', transformOrigin: 'bottom center' }} />
               )}
               {ringsImage && (
                 <img src={ringsImage} alt="" aria-hidden className="absolute -right-8 sm:-right-10 -bottom-6 w-28 sm:w-32 object-contain pointer-events-none" />
